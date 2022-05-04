34c9523bded9f055c95c1828103170ff7b590bb4
Corey McLaughlin <clmclaughlin95@hotmail.com>
2022-05-04 12:17:15 -0400

    Created a giberish module with unit tests

--- src/gibberish.js
+++ src/gibberish.js
@@ -1,45 +1,75 @@
+'use strict'
+
 /**
- * Type: Function
+ * A module used to randomize characters of a string. This is extremely useful for checking 
+ * translations within a user interface.
  * 
- * Description: Is a function that will translate a string into a gibberish 
- * string. This is extremely useful for checking translation texts for user
- * interfaces. It may have its purpose else where too.
- * 
- * Notes:
- * * Will preserve spaces
+ * Type: Module
  * 
  * Author: Corey Lee McLaughlin
- * 
- * @example
- * var text = 'The cow jumped over the moon';
- * gibberish(text);
- * // Returns
- * "flg wce 79pqqa 4hc3 vjg 9mgp"
- * 
- * @param { String } text is the string that we want to translate into gibberish
- * @throws { String } will throw an error if text parameter is not a string
- * @returns { String } will return a gibberish translated string
  */
-export function gibberish(text) {
-	let generalError = 'gibberish() Error: ';
-	if (typeof text != 'string') {
-		throw generalError + 'Invalid \'text\' parameter';
+let gibberish = (() => {
+
+	/**
+	 * A string containing a list of clean characters.
+	 * 
+	 * Isolated as a private variable to prevent variable reinstantion when
+	 * getRandomCharacter() is called
+	 * 
+	 * Type: Private Function
+	 * 
+	 * Author: Corey Lee McLaughlin
+	 * 
+	 */
+	let characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
+
+	/**
+	 * Will generate a random character
+	 * 
+	 * Numbers: 0-9
+	 * Captial Letters: A-Z
+	 * Lower Case Letters: a-z
+	 * 
+	 * Type: Public Function
+	 * 
+	 * Author: Corey Lee McLaughlin
+	 * 
+	 * @returns {string} a random character 
+	 */
+	function getRandomCharacter() {
+		return characters[Math.floor(Math.random() * characters.length)];
 	}
-	let response = '';
-	for (let i = 0; i < text.length; i++) {
-		if (text[i] == '%' && text[i + 1] == 's') {
-			response += '%s';
-			i++;
-			continue;
+	
+	/**
+	 * Randomizes characters of a string with number, upper case or lower case letters while
+	 * preserving spaces.
+	 * 
+	 * Type: Public Function
+	 * 
+	 * Author: Corey Lee McLaughlin
+	 * 
+	 * @param {String} text a string of text to replace
+	 * @throws {String} will throw an error if text parameter is not a string
+	 * @returns {String} a string of randomized characters
+	 */
+	function translate(text) {
+		if (typeof text != 'string') { throw 'Invalid text'; }
+		let response = '';
+		for (let i = 0; i < text.length; i++) {
+			if (text[i] == ' ') {
+				response += ' ';
+			} else {
+				response += this.getRandomCharacter();
+			}
 		}
-		if (text[i] != ' ') {
-			let characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
-			let randomNumber = Math.floor(Math.random() * characters.length);
-			let character = characters.substring(randomNumber, randomNumber + 1);
-			response += character;
-		} else {
-			response += ' ';
-		}
+		return response;
 	}
-	return response;
-};
+
+	return {
+		getRandomCharacter: getRandomCharacter,
+		translate: translate
+	};
+
+})();
+
+export default gibberish;
--- test/gibberish.spec.js
+++ test/gibberish.spec.js
@@ -1,10 +1,63 @@
-import { gibberish } from '../src/gibberish.js';
+import gibberish from '../src/gibberish.js';
 
-describe('gibberish()', () => {
+describe('gibberish', () =>{
 
-    it('Test A', () => {
-        gibberish('test');
-        expect(true).toEqual(true);
+    describe('randomCharacter', () => {
+    
+        it('Returns string', () => {
+            let response = gibberish.getRandomCharacter('t');
+            expect(typeof response).toEqual('string');
+        });
+    
+        it('Replaces character with something random', () => {
+            let character = '#';
+            let response = gibberish.getRandomCharacter(character);
+            expect(response).not.toEqual(character);
+        });
+    
+    });
+    
+    describe('gibberish.translate()', () => {
+    
+        let errorMessage = 'Invalid text';
+    
+        it('Undefined text parameter', () => {
+            try {
+                gibberish.translate();
+                throw 'Allowed an undefined text parameter';
+            } catch (err) {
+                expect(err).toEqual(errorMessage);
+            }
+        });
+    
+        it('Invalid text parameter data type', () => {
+            try {
+                gibberish.translate(1234);
+                throw 'Allowed an invalid text parameter data type';
+            } catch (err) {
+                expect(err).toEqual(errorMessage);
+            }
+        });
+    
+        it('Returns string', () => {
+            let response = gibberish.translate('test');
+            expect(typeof response).toEqual('string');
+        });
+    
+        it('String is randomized', () => {
+            let text = '@#$';
+            let response = gibberish.translate(text);
+            expect(response).not.toEqual(text);
+        });
+
+        it('Preserve Spaces', () => {
+            let text = '0123 5678';
+            expect(text[4]).toEqual(' ');
+            let response = gibberish.translate(text);
+            expect(response[4]).toEqual(' ');
+
+        });
+    
     });
 
 });
